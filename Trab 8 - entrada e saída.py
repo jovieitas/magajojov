0226ef1a690fca814c07f37f8e9af641e1ead5f5
Joana <159146020+jovieitas@users.noreply.github.com>
2024-04-30 10:42:06 -0300

    Add files via upload

---
+++ Trab 8 - entrada e saída.py
@@ -0,0 +1,11 @@
+def infinito():
+    print("Digite números para adicionar à lista (digite 'done' para terminar):")
+    numeros = []
+    while True:
+        entrada = input()
+        if entrada.lower() == 'done':
+            break
+        else:
+            numeros.append(int(entrada))
+    print("Números coletados:", numeros)
+    
